f3d81d3295d2dd69cd94e5a986e6aeada10dfef7
Prabhakar <prabhakarforever44@gmail.com>
2018-08-06 16:52:47 +0530

    loops changed to 121

--- numbers.java
+++ numbers.java
@@ -2,7 +2,7 @@ public class PrintNumbers
 {
     public static void main(String[] args)
     {
-        for(int i=1; i<=12; i++)
+        for(int i=1; i<=121; i++)
         {
             System.out.println(i);
         }
