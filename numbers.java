7f9a0fe5e6de106877dcffc8062a79b941d79e65
Prabhakar <prabhakarforever44@gmail.com>
2018-08-06 17:00:23 +0530

    number loop updated

--- numbers.java
+++ numbers.java
@@ -2,7 +2,7 @@ public class PrintNumbers
 {
     public static void main(String[] args)
     {
-        for(int i=1; i<=121; i++)
+        for(int i=1; i<=123; i++)
         {
             System.out.println(i);
         }
