2a9ef24ca8d1da49dab7310c5d08e31bb7f796c0
Prabhakar <prabhakarforever44@gmail.com>
2018-08-07 13:39:02 +0530

    loop modified to 150

--- numbers.java
+++ numbers.java
@@ -2,7 +2,7 @@ public class PrintNumbers
 {
     public static void main(String[] args)
     {
-        for(int i=1; i<=123; i++)
+        for(int i=1; i<=150; i++)
         {
             System.out.println(i);
         }
